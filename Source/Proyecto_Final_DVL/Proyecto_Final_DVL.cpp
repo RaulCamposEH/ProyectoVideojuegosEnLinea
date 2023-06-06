8028a01b8f29f00e57a942849d3ff5c87b96766d
SKR57 <luis.castanedalms@uanl.edu.mx>
2023-06-05 23:49:41 -0600

    Implementacion API Steam
    Steam fue conectado exitosamente al proyecto

---
+++ Source/Proyecto_Final_DVL/Proyecto_Final_DVL.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Proyecto_Final_DVL.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Proyecto_Final_DVL, "Proyecto_Final_DVL" );
